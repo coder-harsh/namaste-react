869729746ec7db82ef255845fb4c854e5638533d
Harsh <harshj7.net@gmail.com>
2024-03-03 04:39:55 +0530

    dd tut 3 note till 92

--- App.js
+++ App.js
@@ -7,14 +7,25 @@ const heading = React.createElement("h2", { id: "heading2" }, "This is react hea
 console.log(heading); //{$$typeof: Symbol(react.element), type: 'h2', key: null, ref: null, props: {…}, …}: object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading); //this heading will overwrite inner h1
-*/
 
-const heading = React.createElement("h2", { id: "heading", className: "h2" }, "H2 heading");
+
+const heading = React.createElement("h2", { id: "heading", className: "h2" }, "H2 heading"); //create react element using core react
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
-
+*/
 //dom element are html element
 //React Element: it is an element equivalent to dom element
 // react element is an oobject
+const jsxheading = <h1 id="heading">Hello Using JSX🚀</h1>//jsx //jsx is diff then html //this code is eq to react.createElement code //create react element using jsx
 
+//jsx is html like syntax //xml like syntax
+
+//jsx is react elelemnet? no it's a syntax
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxheading);
+console.log(jsxheading); //object same as react.createElement
+
+
+//now we will use jsx
 
