434fc8f9277993ab3598de3e2aab1a5cae18ee00
Harsh <harshj7.net@gmail.com>
2024-03-04 02:04:05 +0530

    add -tut 3 note till line 204

--- App.js
+++ App.js
@@ -52,5 +52,5 @@ const HeadingComponent = () => {
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(HeadingComponent);
+root.render(<HeadingComponent />); //Hello from fun component
 
